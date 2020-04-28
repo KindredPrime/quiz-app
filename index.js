5bd3e6d711804118e97ab2808123502559ffac55
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-04-28 17:47:29 -0400

    Created an object in index.js to store the quiz content

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function handleQuiz() {
+function handleQuiz(quizQuestions) {
     // Handles starting the quiz when the user clicks the "Start Quiz" button
     function handleQuizStart() {
         console.log("handleQuizStart function ran");
@@ -25,4 +25,38 @@ function handleQuiz() {
     handleQuizRestart();
 }
 
-$(handleQuiz());
+$(handleQuiz([
+    {
+        question: "Who is the author of A Song of Ice and Fire?",
+        answerOptions: ["J. R. R. Tolkien", "George R. R. Martin", "J. K. Rowling", "Jon Snow"],
+        correctAnswer: "George R. R. Martin"
+    }, {
+        question: "What is the name of the third book in the Song of Ice and Fire book series?",
+        answerOptions: ["A Hail of Halberds", "A Monsoon of Maces", "A Storm of Swords", "A Downpour of Duels"],
+        correctAnswer: "A Storm of Swords"
+    }, {
+        question: "What is the name of the headquarters of the Night's Watch?",
+        answerOptions: ["Castle Black", "Eastwatch by the Sea", "Winterfell", "Hardhome"],
+        correctAnswer: "Castle Black"
+    }, {
+        question: "What is the name of the throne of Westeros?", 
+        answerOptions: ["The Iron Throne", "The Steel Seat", "The Chair of China", "Hot Pie"],
+        correctAnswer: "The Iron Throne"
+    }, {
+        question: "What is the name of Jon Snow's direwolf?",
+        answerOptions: ["Snow", "Rain", "Ghost", "Casper"],
+        correctAnswer: "Ghost"
+    }, {
+        question: "Where is Tyrion Lannister from?",
+        answerOptions: ["Casterly Rock", "Lannisport", "King's Landing", "Winterfell"],
+        correctAnswer: "Casterly Rock"
+    }, {
+        question: "What is the name of Ned Stark's sister?", 
+        answerOptions: ["Lyanna", "Daenerys", "Meera", "Catelyn"],
+        correctAnswer: "Lyanna"
+    }, {
+        question: "What is the name of the body of water that separates Westeros from Essos?", 
+        answerOptions: ["The Dividing Sea", "The Wide Sea", "The Calm Sea", "The Narrow Sea"],
+        correctAnswer: "The Narrow Sea"
+    }
+]));
