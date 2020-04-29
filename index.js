827f089a1353c0a1d35f671df5790c013a9addac
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-04-28 20:48:48 -0400

    Start Quiz button now loads the first question of the quiz

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 function handleQuiz(quizQuestions) {
+
+    // Renders the question and score trackers on the page
+    function renderTrackers() {
+        $("header").append(`<h2 class="question-number">Question: 1/8</h2><h2 class="score">Score: 0/8</h2>`);
+    }
+
+    // Renders the question found at the provided index in quizQuestions
+    function renderQuestion(questionNumber) {
+        let question = quizQuestions[questionNumber].question;
+        let answerOptions = quizQuestions[questionNumber].answerOptions;
+        let name = quizQuestions[questionNumber].htmlRadioName;
+
+        $(".js-quiz-form").html(`<fieldset><legend>${question}</legend><input type="radio" id=${answerOptions[0].htmlID} name=${name} value=${answerOptions[0].htmlID}><label for=${answerOptions[0].htmlID}>${answerOptions[0].answer}</label><br><input type="radio" id=${answerOptions[1].htmlID} name=${name} value=${answerOptions[1].htmlID}><label for=${answerOptions[1].htmlID}>${answerOptions[1].answer}</label><br><input type="radio" id=${answerOptions[2].htmlID} name=${name} value=${answerOptions[2].htmlID}><label for=${answerOptions[2].htmlID}>${answerOptions[2].answer}</label><br><input type="radio" id=${answerOptions[3].htmlID} name=${name} value=${answerOptions[3].htmlID}><label for=${answerOptions[3].htmlID} class="last-answer">${answerOptions[3].answer}</label></fieldset><button type="submit" class="js-submit-answer">Submit</button>`);
+    }
+    
     // Handles starting the quiz when the user clicks the "Start Quiz" button
     function handleQuizStart() {
-        console.log("handleQuizStart function ran");
+        let quizStartLayout;
+
+        $(".js-quiz-start").on("click", event => {
+            event.preventDefault();
+            console.log("handleQuizStart function ran");
+
+            // Hold on to the HTML content that was removed from the page, so I can add it back in later
+            quizStartLayout = $(".js-quiz-form").children().detach();
+
+            // Render the Question and Score trackers
+            renderTrackers();
+
+            // Render the first question
+            renderQuestion(0);
+        });
+
+        return quizStartLayout;
     }
 
     // Handles checking the user's answer to a question when they click the "Submit" button
@@ -19,7 +50,7 @@ function handleQuiz(quizQuestions) {
         console.log("handleQuizRestart function ran");
     }
 
-    handleQuizStart();
+    let startLayout = handleQuizStart();
     handleAnswerSubmit();
     handleNextLayout();
     handleQuizRestart();
@@ -28,8 +59,22 @@ function handleQuiz(quizQuestions) {
 $(handleQuiz([
     {
         question: "Who is the author of A Song of Ice and Fire?",
-        answerOptions: ["J. R. R. Tolkien", "George R. R. Martin", "J. K. Rowling", "Jon Snow"],
-        correctAnswer: "George R. R. Martin"
+        answerOptions: [
+            {
+                answer: "J. R. R. Tolkien",
+                htmlID: "tolkien"
+            }, {
+                answer: "George R. R. Martin",
+                htmlID: "martin"
+            }, {
+                answer: "J. K. Rowling",
+                htmlID: "rowling"
+            }, {
+                answer: "Jon Snow",
+                htmlID: "snow"
+        }],
+        correctAnswer: "George R. R. Martin",
+        htmlRadioName: "author"
     }, {
         question: "What is the name of the third book in the Song of Ice and Fire book series?",
         answerOptions: ["A Hail of Halberds", "A Monsoon of Maces", "A Storm of Swords", "A Downpour of Duels"],
